fbd4803bd73991003344b3603cf8c4445e74331c
Refrijirator <77860777+Refrijirator@users.noreply.github.com>
2025-06-12 15:42:45 +0300

    Update eval.py
    eval_model function from notebook

--- src/eval.py
+++ src/eval.py
@@ -1,13 +1,36 @@
 # Evaluation functions are model-agnostic
 
-def test_step(model, data_loader, loss_fn, accuracy_fn, scheduler=None):
-    # Paste your test_step here
-    pass
+def eval_model(model: torch.nn.Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               accuracy_fn,
+               device: torch.device = device):
+    """Evaluates a given model on a given dataset.
 
-def eval_step(model, data_loader, loss_fn, accuracy_fn):
-    # Paste your eval_step here
-    pass
+    Args:
+        model (torch.nn.Module): A PyTorch model capable of making predictions on data_loader.
+        data_loader (torch.utils.data.DataLoader): The target dataset to predict on.
+        loss_fn (torch.nn.Module): The loss function of model.
+        accuracy_fn: An accuracy function to compare the models predictions to the truth labels.
+        device (str, optional): Target device to compute on. Defaults to device.
 
-def eval_loop(model, data_loader, loss_fn, optimizer, accuracy_fn, num_epochs=None, scheduler=None):
-    # Paste your eval_loop here
-    pass
+    Returns:
+        (dict): Results of model making predictions on data_loader.
+    """
+    loss, acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            # Send data to the target device
+            X, y = X.to(device), y.to(device)
+            y_pred = model(X).squeeze(dim=1)
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true=y,
+                                y_pred=y_pred.round())
+
+        # Scale loss and acc
+        loss /= len(data_loader)
+        acc /= len(data_loader)
+    return {"model_name": model.__class__.__name__, # only works when model was created with a class
+            "model_loss": loss.item(),
+            "model_acc": acc}
